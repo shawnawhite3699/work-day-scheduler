cfdfe50ea9c35323624c90e70386288dd1bc4e22
Shawna White <sw1056@gmail.com>
2022-10-13 17:58:03 -0500

    added save/refresh functionality to JS, changed present CSS color

--- script.js
+++ script.js
@@ -37,8 +37,6 @@ function timeblockColor() {
     })
 };
 
-timeblockColor ();
-
 //WHEN I click the save button for that timeblock
 //THEN the text for that event is saved in local storage
 $(document).ready(function () {
@@ -49,3 +47,29 @@ $(document).ready(function () {
         localStorage.setItem(time, event);
     });
 });
+
+//WHEN I refresh the page
+//THEN the saved events persist
+//function useScheduler() {
+    //$(".hour").each(function() {
+        //var currentHour = $(this).text();
+        //var currentSchedule = localStorage.getItem(currentHour);
+
+        //if(currentSchedule !== null) {
+            //$(this).siblings(".description").val(currentSchedule);
+        //}
+    //});
+//};
+
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#1 .description").val(localStorage.getItem("1"));
+    $("#2 .description").val(localStorage.getItem("2"));
+    $("#3 .description").val(localStorage.getItem("3"));
+    $("#4 .description").val(localStorage.getItem("4"));
+    $("#5 .description").val(localStorage.getItem("5"));
+
+    timeblockColor ();
+//useScheduler ();
