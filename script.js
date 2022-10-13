ebd1a8d2706bc1169ec68cb71a5b34e936badd69
Shawna White <sw1056@gmail.com>
2022-10-13 18:26:55 -0500

    partly fixed color coding, still need to add an interval to call the function

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-//WHEN I open the planner
-//THEN the current day is displayed at the top of the calendar
-var currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd, MMMM D, YYYY"));
-
-//WHEN I view the timeblocks for that day
-//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-function timeblockColor() {
-    var hour = moment().hour();
-
-    $(".time-block").each(function() {
-        var currentHour = parseInt($(this).attr("id"));
-        
-        if (currentHour < hour) {
-            $(this).addClass("past");
-        } else if (currentHour === hour) {
-            $(this).addClass("present");
-        } else {
-            $(this).addClass("future");
-        }
-
-        //if (currentHour < hour) {
-            //$(this).removeClass("future");
-            //$(this).removeClass("present");
-            //$(this).addClass("past");
-       //}
-        //else if (currentHour === hour) {
-            //$(this).removeClass("past");
-            //$(this).removeClass("future");
-            //$(this).addClass("present");
-        //}
-        //else {
-            //$(this).removeClass("present");
-            //$(this).removeClass("past");
-            //$(this).addClass("future");
-        //}
-    })
-};
+//I need an interval somewhere to make the color coding work. Interval needs to call timeBlockColor function.
 
 //WHEN I click the save button for that timeblock
 //THEN the text for that event is saved in local storage
@@ -46,21 +9,32 @@ $(document).ready(function () {
 
         localStorage.setItem(time, event);
     });
-});
+
+//WHEN I view the time blocks for that day
+//THEN each time block is color coded to indicate whether it is in the past, present, or future
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id").split("-")[1]);
+        
+        if (currentHour < hour) {
+            $(this).addClass("past");
+        } else if (currentHour === hour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present")
+            $(this).addClass("future");
+        }
+    })
+};
+
+timeBlockColor();
 
 //WHEN I refresh the page
 //THEN the saved events persist
-//function useScheduler() {
-    //$(".hour").each(function() {
-        //var currentHour = $(this).text();
-        //var currentSchedule = localStorage.getItem(currentHour);
-
-        //if(currentSchedule !== null) {
-            //$(this).siblings(".description").val(currentSchedule);
-        //}
-    //});
-//};
-
     $("#9 .description").val(localStorage.getItem("9"));
     $("#10 .description").val(localStorage.getItem("10"));
     $("#11 .description").val(localStorage.getItem("11"));
@@ -71,5 +45,8 @@ $(document).ready(function () {
     $("#4 .description").val(localStorage.getItem("4"));
     $("#5 .description").val(localStorage.getItem("5"));
 
-    timeblockColor ();
-//useScheduler ();
+//WHEN I open the planner
+//THEN the current day is displayed at the top of the calendar
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("dddd, MMMM D, YYYY"));
+});
