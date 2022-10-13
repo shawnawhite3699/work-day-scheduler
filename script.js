3b85369614a700ba56edf80a3247f5adcc77bad3
Shawna White <sw1056@gmail.com>
2022-10-13 16:34:59 -0500

    updated HTML, CSS, and added timeblock color coding function to JS - not functioning correctly yet

--- script.js
+++ script.js
@@ -2,3 +2,23 @@
 //THEN the current day is displayed at the top of the calendar
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM D, YYYY"));
+
+//WHEN I view the timeblocks for that day
+//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+function timeblockColor() {
+    var hour = moment().hour();
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+        
+        if (currentHour < hour) {
+            $(this).addClass("past");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+    })
+};
+
+timeblockColor ();
